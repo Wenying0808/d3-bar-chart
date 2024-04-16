c04a270fecfbfea896de538c784d86787a899814
Wenying Chang <changwenyingnl@gmail.com>
2024-04-16 20:30:37 +0200

    add files

---
+++ script.js
@@ -0,0 +1,14 @@
+// set up the dimension for the chart
+
+// create svg container
+
+// load and process data
+
+// set the x and y scales
+
+// create x and y axes
+
+// add x and y axes to the chart
+
+
+//create the bars for chart
